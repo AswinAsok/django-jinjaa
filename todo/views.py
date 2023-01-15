4bdfc30d9543bb15339b260f6a18184fc6075188
Aswin Asok <aswinasokofficial@gmail.com>
2023-01-15 13:25:38 +0530

    develop:project-setup-done

---
+++ todo/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Todo
+
+
+# Create your views here
+# .
+
+def index(request):
+
+    todos = Todo.objects.all()
+
+    return render(request, 'index.html', {'todos': todos})
