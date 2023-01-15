c6b08deeb180aac0931f5a7762a51a48d1a7b39d
Aswin Asok <aswinasokofficial@gmail.com>
2023-01-15 14:15:50 +0530

    develop:create-todo-form-working

--- todo/views.py
+++ todo/views.py
@@ -10,3 +10,23 @@ def index(request):
     todos = Todo.objects.all()
 
     return render(request, 'index.html', {'todos': todos})
+
+
+def create(request):
+    if request.method == 'GET':
+        return render(request, 'create.html')
+
+    elif request.method == 'POST':
+        title = request.POST['title']
+        description = request.POST['description']
+        date = request.POST['date']
+        completed = False
+        print(date)
+        todo = Todo.objects.create(
+            title=title, description=description, completiondate=date, completed=completed)
+
+        todo.save()
+
+        todos = Todo.objects.all()
+
+        return render(request, 'index.html', {'todos': todos})
